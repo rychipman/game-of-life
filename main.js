01109f3241ee6229b608c23c5a8eac05cc916726
Ryan Chipman <rchipman@mit.edu>
2014-09-23 23:45:59 -0400

    project finally done

--- main.js
+++ main.js
@@ -10,28 +10,33 @@ lifeApp.autorun = function() {
     return game;
 };
 
+// an object for storing the user's saved states
 lifeApp.saved_states = {
     glider_gun : [[0,4],[0,5],[1,4],[1,5], [10,4],[10,5],[10,6],[11,3],[11,7],[12,2],[12,8],[13,2],[13,8],[14,5],[15,3],[15,7],[16,4],[16,5],[16,6],[17,5], [20,2],[20,3],[20,4],[21,2],[21,3],[21,4],[22,1],[22,5],[24,0],[24,1],[24,5],[24,6], [34,2],[34,3],[35,2],[35,3]]
 };
 
 $(document).ready(function() {
 
-    game = lifeApp.autorun();
+    var game = lifeApp.autorun();
 
+    // Run game when play clicked
     $('#play-button').click(function(event) {
 	game.run();
     });
 
+    // Pause game when pause clicked
     $('#pause-button').click(function(event) {
 	game.pause();
     });
 
+    // save the current state of the board
     $('#save-button').click(function(event) {
 	var state = game.grabBoard();
 	var name = prompt("Name the saved state:", "savedState1");
 	lifeApp.saved_states[name] = state;
     });
 
+    // restore a particular saved state
     $('#restore-button').click(function(event) {
 	var prompt_text = "Type the name of the state to restore.\nThe following saved states are available:\n";
 	for(var state in lifeApp.saved_states)
@@ -39,8 +44,9 @@ $(document).ready(function() {
 	var selected_state = prompt(prompt_text);
 	var board = lifeApp.saved_states[selected_state];
 	game.restoreBoard(board);
-    })
+    });
 
+    // toggle a call on/off when clicked
     $('.cell').click(function(event) {
 	var cell = $(this);
 	var x = cell.data('col');
